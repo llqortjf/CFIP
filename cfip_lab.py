c0f9d4ca17759d07af45fc81baa6cb0ca04b885f
llqortjf <141333365+llqortjf@users.noreply.github.com>
2026-05-14 11:03:37 +0900

    Update cfip_lab.py

--- cfip_lab.py
+++ cfip_lab.py
@@ -54,7 +54,7 @@ sorted_counts = counts.most_common()
 color= np.zeros((1,cluster_num,3), dtype=np.uint8)
 
 for i in range(cluster_num):
-  color[0, i ] = rgb[ sorted_counts[i][0] ]
+  color[:, i ] = rgb[ sorted_counts[i][0] ]
 
 plt.imshow(color)
 plt.show()
